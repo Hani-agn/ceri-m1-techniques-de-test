9015f78f5638390166ff30e219e8aa08a0b7b5a1
Hani <Agguinihani.off@gmail.com>
2024-03-12 17:29:18 +0100

    config de codecov

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -2,6 +2,8 @@ package fr.univavignon.pokedex.api;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
