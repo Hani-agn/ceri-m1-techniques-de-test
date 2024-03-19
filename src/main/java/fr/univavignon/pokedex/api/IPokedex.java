5bd586b0e7d284de02923d7049bf293850ea50bb
Hani <Agguinihani.off@gmail.com>
2024-03-19 16:27:30 +0100

    changement des test pour IPokedexTest

--- src/main/java/fr/univavignon/pokedex/api/IPokedex.java
+++ src/main/java/fr/univavignon/pokedex/api/IPokedex.java
@@ -17,6 +17,7 @@ public interface IPokedex extends IPokemonMetadataProvider, IPokemonFactory {
 	 * @return Number of pokemon in this pokedex.
 	 */
 	int size();
+
 	
 	/**
 	 * Adds the given <tt>pokemon</tt> to this pokedex and returns
--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -18,14 +18,15 @@ public class IPokedexTest {
 
     @BeforeEach
     public void setUp() throws PokedexException {
-        pokedex = mock(IPokedex.class);
+        IPokemonMetadataProvider pokemonMetadataProvider = mock(IPokemonMetadataProvider.class);
+        IPokemonFactory pokemonFactory = mock(IPokemonFactory.class);
+        pokedex = new Pokedex(pokemonMetadataProvider, pokemonFactory);
         pokemonMetadata = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
     }
 
     @Test
     public void testSize() throws PokedexException {
-        when(pokedex.size()).thenReturn(1);
-        assertEquals(1, pokedex.size());
+        assertEquals(0, pokedex.size());
     }
 
     @Test
@@ -42,8 +43,8 @@ public class IPokedexTest {
                 4, // candy
                 56 // iv
         );
-        when(pokedex.addPokemon(pokemon)).thenReturn(1);
-        assertEquals(1, pokedex.addPokemon(pokemon));
+        assertEquals(0, pokedex.addPokemon(pokemon));
+        assertEquals(1, pokedex.size());
     }
 
     @Test
@@ -60,8 +61,8 @@ public class IPokedexTest {
                 4, // candy
                 56 // iv
         );
-        when(pokedex.getPokemon(1)).thenReturn(expectedPokemon);
-        assertEquals(expectedPokemon, pokedex.getPokemon(1));
+        pokedex.addPokemon(expectedPokemon);
+        assertEquals(expectedPokemon, pokedex.getPokemon(0));
     }
 
     @Test
@@ -81,8 +82,8 @@ public class IPokedexTest {
                     56 // iv
             );
             pokemonList.add(pokemon);
+            pokedex.addPokemon(pokemon);
         }
-        when(pokedex.getPokemons()).thenReturn(pokemonList);
         assertEquals(pokemonList, pokedex.getPokemons());
     }
 
@@ -103,10 +104,10 @@ public class IPokedexTest {
                     56 // iv
             );
             pokemonList.add(pokemon);
+            pokedex.addPokemon(pokemon);
         }
         Comparator<Pokemon> comparator = Comparator.comparing(Pokemon::getName);
         pokemonList.sort(comparator);
-        when(pokedex.getPokemons(comparator)).thenReturn(pokemonList);
         assertEquals(pokemonList, pokedex.getPokemons(comparator));
     }
 }
